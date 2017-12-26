f3b1c427d3a277588c8d53b862d8680eedf4d6b5
zeromake <a390720046@gmail.com>
2017-12-26 09:44:10 +0800

    fix: test android is router hash

--- src/router.tsx
+++ src/router.tsx
@@ -5,7 +5,7 @@ import Animate from "preact-animate";
 
 const MainRouter = () => (
     <Router>
-        <Animate component="div" componentProps={{className: "main"}} transitionEnter={true} transitionLeave={true} transitionName={null}>
+        <Animate component="div" componentProps={{className: "main", style: "width: 100%; height: 100%;"}} transitionEnter={true} transitionLeave={true} transitionName={null}>
             <Route key="1" component={Library}  path="/" transitionName={{ enter: "fadeInLeft", leave: "fadeOutLeft" }}/>
             <Route key="2" component={BookLayout} path="/library/:sha/" transitionName={{ enter: "fadeInRight", leave: "fadeOutRight" }}/>
         </Animate>
